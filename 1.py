d07acd4d9d647b56e64db4fd770df0c23bfb5712
Biniyam Awalachew Firde <biniyam22awlachew@gmail.com>
2025-07-28 22:03:15 +0200

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,10 @@
+#Two Sum
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_dict = {}
+        for index, num in enumerate(nums):
+            needed_num = target - num
+            if needed_num in num_dict:
+                return [num_dict[needed_num], index]
+            num_dict[num] = index
+        return []
