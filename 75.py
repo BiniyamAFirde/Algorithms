c822c543719139ec7c6d39ca335806089ba5636c
Biniyam Awalachew Firde <biniyam22awlachew@gmail.com>
2025-08-14 00:02:42 +0200

    Create 75.py

---
+++ 75.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        for i in range(1,len(nums)): 
+            key=nums[i]
+            j=i
+            while j>0 and nums[j-1]>key:
+                nums[j]=nums[j-1]
+                j-=1
+                nums[j]=key
