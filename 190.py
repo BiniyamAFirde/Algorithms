aea20b9775da1e52a22bfe97b11589a2151f5fc9
Biniyam Awalachew Firde <biniyam22awlachew@gmail.com>
2025-10-01 23:29:07 +0200

    Update 190.py

--- 190.py
+++ 190.py
@@ -2,7 +2,6 @@ class Solution:
     def reverseBits(self, n: int) -> int:
         result = 0
         for i in range(32):
-     
             result <<= 1
             
             result |= n & 1
