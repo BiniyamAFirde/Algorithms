7cd449cca2c089015f464f3ddf6032a2b7e29b05
Biniyam Awalachew Firde <biniyam22awlachew@gmail.com>
2025-10-02 23:02:48 +0200

    Update 918.py

--- 918.py
+++ 918.py
@@ -4,7 +4,6 @@ class Solution:
         current_min = min_so_far = nums[0]
         total_sum = nums[0]
         for i in range(1, len(nums)):
-
             current_max = max(nums[i], current_max + nums[i])
             max_so_far = max(max_so_far, current_max)
             
