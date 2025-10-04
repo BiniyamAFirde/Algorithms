f8ef1b3095c5ac811eadce8ebd30657ed6424427
Biniyam Awalachew Firde <biniyam22awlachew@gmail.com>
2025-10-04 23:10:09 +0200

    53.py

--- 53.py
+++ 53.py
@@ -3,12 +3,10 @@ class Solution:
         if not nums:
             return 0
         max_sum = nums[0]
-        current_sum = 0
-        
+        current_sum = 0  
         for num in nums:
             if current_sum < 0:
                 current_sum = 0
             current_sum += num
-            max_sum = max(max_sum, current_sum)
-        
+            max_sum = max(max_sum, current_sum) 
         return max_sum
