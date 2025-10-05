b1c584f5fd7eb5438f2e5c4f0261263c4966b8e5
Biniyam Awalachew Firde <biniyam22awlachew@gmail.com>
2025-10-05 23:43:36 +0200

    1343.py

--- 1343.py
+++ 1343.py
@@ -3,8 +3,10 @@ class Solution:
         target = threshold * k
         current_sum = sum(arr[:k])
         count = 1 if current_sum >= target else 0
+        
         for i in range(k, len(arr)):
             current_sum += arr[i] - arr[i - k]
             if current_sum >= target:
                 count += 1
+                
         return count
