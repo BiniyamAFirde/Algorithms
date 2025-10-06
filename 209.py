079f66495329fd2d3f96ec20c0f6ccb3fd0f53a5
Biniyam Awalachew Firde <biniyam22awlachew@gmail.com>
2025-10-07 00:56:36 +0200

    209.py

--- 209.py
+++ 209.py
@@ -1,3 +1,4 @@
+
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
         left = 0
@@ -6,6 +7,7 @@ class Solution:
         
         for right in range(len(nums)):
             current_sum += nums[right]
+            
             while current_sum >= target:
                 min_length = min(min_length, right - left + 1)
                 current_sum -= nums[left]
