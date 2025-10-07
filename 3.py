72895399c3037d401c0c1ad129ecc0f337cfef02
Biniyam Awalachew Firde <biniyam22awlachew@gmail.com>
2025-10-07 23:12:13 +0200

    Update 3.py

--- 3.py
+++ 3.py
@@ -3,7 +3,6 @@ class Solution:
         char_set = set()  
         left = 0 
         max_length = 0
-
         for right in range(len(s)):  
             while s[right] in char_set:
                 char_set.remove(s[left])
