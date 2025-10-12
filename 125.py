92489d5d9a0ef7258d8d17f513be9cf43f4fbe18
Biniyam Awalachew Firde <biniyam22awlachew@gmail.com>
2025-10-12 11:22:53 +0200

    Create 125.py

---
+++ 125.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        if s=="":
+            return True
+        new=""
+        for ch in s:
+            if ch.isalnum()==True:
+                new+=ch.lower()
+        b=""
+        for i in range(len(new)-1,-1,-1):
+                b+=new[i]
+        if b==new:
+            return True
+        return False
