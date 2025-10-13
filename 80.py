b1befff57bc3a04a68a1fef3535c4fe8169c18a0
Biniyam Awalachew Firde <biniyam22awlachew@gmail.com>
2025-10-13 11:02:26 +0200

    Update 80.py

--- 80.py
+++ 80.py
@@ -4,7 +4,7 @@ class Solution:
             return len(nums)
         l=2
         for i in range(2,len(nums)):
-            if nums[i]!=nums[l-2]:
+            if nums[i]!=nums[l-2]: #Key
                 nums[l]=nums[i]
                 l+=1
         return l
