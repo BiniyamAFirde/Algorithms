6e782c33d8b95b5aa521cfce1170979ee66ea1a6
Biniyam Awalachew Firde <biniyam22awlachew@gmail.com>
2025-10-22 23:57:44 +0200

    211.py
    Implement a WordDictionary with add and search methods using a Trie structure.

---
+++ 211.py
@@ -0,0 +1,43 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_end = True
+
+    def search(self, word: str) -> bool:
+        def dfs(node, index):
+            if index == len(word):
+                return node.is_end
+            
+            char = word[index]
+            
+            if char == '.':
+                for child in node.children.values():
+                    if dfs(child, index + 1):
+                        return True
+                return False
+            else:
+                if char not in node.children:
+                    return False
+                return dfs(node.children[char], index + 1)
+        
+        return dfs(self.root, 0)
+        
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
