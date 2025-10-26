b377e06c0ff8b28c3353ab0e82df84bfadcab174
Biniyam Awalachew Firde <biniyam22awlachew@gmail.com>
2025-10-26 22:49:41 +0100

    684.py

--- 684.py
+++ 684.py
@@ -1,38 +1,28 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-
-        parent = [i for i in range(len(edges) + 1)]
-    
+        parent = [i for i in range(len(edges) + 1)]   
         def find(x):
             if parent[x] != x:
                 parent[x] = find(parent[x])
-            return parent[x]
-      
+            return parent[x]      
         def union(x, y):
             root_x = find(x)
-            root_y = find(y)
-            
+            root_y = find(y)            
             if root_x == root_y:
-                return False  
-        
+                return False          
             parent[root_y] = root_x
             return True  
-
         for edge in edges:
             u, v = edge
          
             if not union(u, v):
-                return edge
-        
+                return edge  
         return []  
-
 if __name__ == "__main__":
     sol = Solution()
-
     edges1 = [[1,2],[1,3],[2,3]]
     print(f"Input: {edges1}")
     print(f"Output: {sol.findRedundantConnection(edges1)}") 
-    
     print()
     edges2 = [[1,2],[2,3],[3,4],[1,4],[1,5]]
     print(f"Input: {edges2}")
